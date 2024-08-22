4516b7fee76e6212c8df57c851c969d2ddafb508
Siddharth Rao <siddrao.926@gmail.com>
2024-08-22 05:02:24 -0700

    Added Json Models To Frontend

---
+++ frontend/src/models/projects.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+const ProjectSchema = z.object({
+	name: z.string(),
+	bulletPoints: z.array(z.string()),
+});
+
+export type Project = z.infer<typeof ProjectSchema>;
---
+++ frontend/src/models/skills.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+const SkillsSchema = z.object({
+	category: z.string(),
+	skills: z.array(z.string()),
+});
+
+export type Skills = z.infer<typeof SkillsSchema>;
