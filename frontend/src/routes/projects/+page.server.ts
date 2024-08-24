e47861d9198ce06d135fe2d5da80cc952f5e1f55
Siddharth Rao <siddrao.926@gmail.com>
2024-08-24 11:24:26 -0700

    Added Svelte Project Page

---
+++ frontend/src/routes/projects/+page.server.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import type { Project } from "../../types/projects";
+import type { PageServerLoad } from "./$types";
+
+export const load = (async () => {
+	const API_URL = import.meta.env.VITE_API_URL;
+
+	if (!API_URL) {
+		throw new Error("API_URL not defined");
+	}
+
+	const response = await axios.get<Project[]>(`${API_URL}/projects`);
+	const projects = response.data;
+
+	return {
+		projects: projects,
+	};
+}) satisfies PageServerLoad;
---
+++ frontend/src/routes/projects/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Layout from "../+layout.svelte";
+	import ProjectSection from "../../components/ProjectSection.svelte";
+	import type { Project } from "../../types/projects.ts";
+
+	export let data: { projects: Project[] };
+</script>
+
+<Layout>
+	<main class="flex flex-col items-start justify-start pt-32 p-12 max-w-4xl mx-auto">
+		<h1 class="title text-5xl font-extrabold tracking-wide">PROJECTS</h1>
+		<section class="mt-12">
+			{#each data.projects as project}
+				<ProjectSection name={project.name} bulletPoints={project.bulletPoints} />
+			{/each}
+		</section>
+	</main>
+</Layout>
+
+<style>
+	.title {
+		color: #e5e7eb;
+		text-shadow:
+			0 0 4px rgba(255, 255, 255, 0.4),
+			0 0 8px rgba(255, 255, 255, 0.2);
+	}
+</style>
