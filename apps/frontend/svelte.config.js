60616c047f92142ce7b3a6b6dfc019b5feab1b2c
Siddharth Rao <siddrao.926@gmail.com>
2024-08-27 23:21:40 -0700

    Swapped SvelteKit Adapter To AWS Amplify

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "amplify-adapter";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,8 +7,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-            runtime: "nodejs20.x",
-        }),
+			runtime: "nodejs20.x",
+		}),
 	},
 };
 
