8c920cb1bf12b7328264f08c4f51b0e14209c6e4
Siddharth Rao <siddrao.926@gmail.com>
2024-09-12 06:55:10 -0700

    Made Base Path Dynamic

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: true,
 		}),
 		paths: {
-			base: process.argv.includes("dev") ? "" : "/portfolio",
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
 		},
 	},
 };
